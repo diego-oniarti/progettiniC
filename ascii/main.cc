4f067cfedf1aca657ea838b69106f12d0b1723e7
diego-oniarti <diego.oniarti@gmail.com>
2021-10-02 02:58:13 +0200

    Commentato ascii

--- ascii/main.cc
+++ ascii/main.cc
@@ -1,3 +1,4 @@
+//Diego Oniarti
 #include <iostream>
 #include <stdint.h>
 using namespace std;
@@ -8,27 +9,35 @@ using namespace std;
 #include <sys/ioctl.h>
 #include <termios.h>
 
+//Stampa a video un immagine
 int main(int argc, char **argv) {
+  //Se non è specificato il nome file, esci
   if (argc<2){
+    cout << "Missing filename"<endl;
     return -1;
   }
   char* fileName = argv[1];
 
-  
+  //dimensioni dell'immagine
   int width, height, bpp;
   uint8_t* rgb_image = stbi_load(fileName, &width, &height, &bpp, 3);
+  //Se l'immagine non viene caricata correttamente, esci
   if (rgb_image==NULL){
+    cout << "Errore di caricamento immagine"<<endl;
     return -1;
   }
 
+  //Ottiene le dimensioni del terminale (colonne e righe)
   struct winsize size;
   ioctl( 0, TIOCGWINSZ, (char *) &size );
   int PWidth = size.ws_col, PHeight = size.ws_row;
 
-  
+  // Decide come scalare l'immagine (il fattore limitante potrebbe essere l'altezza oppure la largezza in base alle proporzioni dell'immagine e del terminale)
   float scalaW = (float)width/(PWidth*0.8F);
   float scalaH = (float)height/(PHeight)/2;
 
+  //Gli step indicano quanti pixel vanno saltati per campionare l'immagine correttamente
+  //Gli step verticali sono il doppio di quelli orizzontali per compensare il fatto che i carattere nel terminale sono più alti che larghi
   int stepY,stepX;
   if (height/scalaW < PHeight*2){
     stepY = scalaW*2;
